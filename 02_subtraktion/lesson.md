2495996f084c78992e1451d7bfbadba575468a7b
schizoakustik <schizoakustik@schizoakustik.se>
2017-08-20 22:04:50 +0200

    Subtraction lesson added

---
+++ 02_subtraktion/lesson.md
@@ -0,0 +1,3 @@
+## Subtraktion (minus)
+
+funktionen 'sub' ska ta två nummer och lämna tillbaka (return) skillnaden. 
---
+++ 02_subtraktion/lesson_tests.py
@@ -0,0 +1,5 @@
+class AdditionTests(unittest.TestCase):
+	def test_sub_is_a_function(self):
+		self.assertTrue(callable(sub))
+	def test_subs_two_positive_numbers(self):
+		self.assertEqual(sub(1, 1), 0)
