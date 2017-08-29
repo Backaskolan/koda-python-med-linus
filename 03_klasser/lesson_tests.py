6b4f84a113c66190e17dfbf4ecaef8cd83ab6559
linus-r-s <31243041+linus-r-s@users.noreply.github.com>
2017-08-29 15:57:10 +0200

    Update lesson_tests.py

--- 03_klasser/lesson_tests.py
+++ 03_klasser/lesson_tests.py
@@ -5,7 +5,7 @@ class ClassTests(unittest.TestCase):
     self.test_instance = Sak('test', 'test')
     
   def test_penna_is_sak(self):
-    self.assertTrue(isinstance(self.test_instance, Sak)
+    self.assertTrue(isinstance(self.test_instance, Sak))
     
   def test_vars_are_strings(self):
     self.assertTrue(type(self.test_instance.namn) == str)
