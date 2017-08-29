bb3537fd8b8d1a518173c4d895e8d4598135a2ee
Linus Rundberg Streuli <schizoakustik@schizoakustik.se>
2017-08-29 19:35:47 +0200

    Flyttade tillbaka subtraktion

---
+++ 02_subtraktion/lesson_tests.py
@@ -0,0 +1,7 @@
+import unittest
+
+class AdditionTests(unittest.TestCase):
+    def test_sub_is_a_function(self):
+        self.assertTrue(callable(sub))
+    def test_subs_two_positive_numbers(self):
+        self.assertEqual(sub(1, 1), 0)
---
+++ 02_subtraktion/main.py
@@ -0,0 +1,2 @@
+def sub(num1, num2):
+    # Skriv din kod här
