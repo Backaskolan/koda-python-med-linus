04eca83d4a347f8041f3290bca2e9af207fa0618
Linus Rundberg Streuli <linus.rundberg@backaskolan.se>
2017-08-30 16:04:08 +0200

    Borde funka nu...

--- 05_input/main.py
+++ 05_input/main.py
@@ -1,7 +1,6 @@
 def add(num1, num2):
   return num1+num2
 
-# Ta bort kommentarerna från de tre sista raderna
-# x = input("Skriv första talet: ")
-# y = input("Skriv andra talet: ")
-# print(add(x, y))
+x = input("Skriv första talet: ")
+y = input("Skriv andra talet: ")
+print(add(x, y))
